5d6ca7af917b7da27ab23da340c96bb113ca1891
likj <jay.likangjun@gmail.com>
2022-09-28 16:28:46 +0800

    fix(demo):修复版本升级带来的多参数解析器的循环依赖问题(参考来自于@feat:dongfangding)

--- eureka-consumer/src/main/java/com/example/eurekaconsumer/processer/MultiArgumentResolverMethodProcessor.java
+++ eureka-consumer/src/main/java/com/example/eurekaconsumer/processer/MultiArgumentResolverMethodProcessor.java
@@ -2,49 +2,43 @@ package com.example.eurekaconsumer.processer;
 
 import com.example.eurekaconsumer.annotation.MultiArgumentResolver;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.MethodParameter;
-import org.springframework.http.converter.*;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.http.converter.support.AllEncompassingFormHttpMessageConverter;
-import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
-import org.springframework.http.converter.xml.SourceHttpMessageConverter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 import org.springframework.web.servlet.mvc.method.annotation.ServletModelAttributeMethodProcessor;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 自定义参数解析器用以支持同一个参数支持application/json和application/x-www-form-urlencoded解析
  *
- * @author Snowball
+ * @author likj
  * @version 1.0
- * @date 2020/08/31 19:00
+ * @date 2022/09/28 19:00
  * @see MultiArgumentResolver
  */
 @Slf4j
+@NoArgsConstructor
 @AllArgsConstructor
-public class MultiArgumentResolverMethodProcessor implements HandlerMethodArgumentResolver {
-
+public class MultiArgumentResolverMethodProcessor implements HandlerMethodArgumentResolver, ApplicationContextAware, SmartInitializingSingleton {
+    private ApplicationContext applicationContext;
+    private RequestMappingHandlerAdapter requestMappingHandlerAdapter;
     private RequestResponseBodyMethodProcessor requestResponseBodyMethodProcessor;
-
     private ServletModelAttributeMethodProcessor servletModelAttributeMethodProcessor;
 
-    public MultiArgumentResolverMethodProcessor() {
-        requestResponseBodyMethodProcessor = new RequestResponseBodyMethodProcessor(initHttpMessageConverters());
-        servletModelAttributeMethodProcessor = new ServletModelAttributeMethodProcessor(true);
-    }
-
     private static final String CONTENT_TYPE_JSON = "application/json";
-
     private static final String CONTENT_TYPE_FORM_URLENCODED = "application/x-www-form-urlencoded";
-
     private static final String CONTENT_TYPE_FORM_DATA = "multipart/form-data";
 
     @Override
@@ -55,11 +49,12 @@ public class MultiArgumentResolverMethodProcessor implements HandlerMethodArgume
     @Override
     public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
         HttpServletRequest httpServletRequest = nativeWebRequest.getNativeRequest(HttpServletRequest.class);
+        assert httpServletRequest != null;
         String contentType = httpServletRequest.getContentType();
-        if (contentType == null) {
+        if (null == contentType) {
             throw new IllegalArgumentException("不支持contentType");
         }
-
+        init();
         if (contentType.contains(CONTENT_TYPE_JSON)) {
             return requestResponseBodyMethodProcessor.resolveArgument(methodParameter, modelAndViewContainer, nativeWebRequest, webDataBinderFactory);
         } else if (contentType.contains(CONTENT_TYPE_FORM_URLENCODED)) {
@@ -70,17 +65,25 @@ public class MultiArgumentResolverMethodProcessor implements HandlerMethodArgume
         throw new IllegalArgumentException("不支持contentType");
     }
 
-    public List<HttpMessageConverter<?>> initHttpMessageConverters() {
-        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
-        messageConverters.add(new StringHttpMessageConverter());
-        messageConverters.add(new ByteArrayHttpMessageConverter());
-        messageConverters.add(new ResourceHttpMessageConverter());
-        messageConverters.add(new ResourceRegionHttpMessageConverter());
-        messageConverters.add(new SourceHttpMessageConverter<>());
-        messageConverters.add(new AllEncompassingFormHttpMessageConverter());
-        messageConverters.add(new MappingJackson2HttpMessageConverter());
-        messageConverters.add(new MappingJackson2XmlHttpMessageConverter());
-        return messageConverters;
+    private void init(){
+        List<HandlerMethodArgumentResolver> argumentResolvers = requestMappingHandlerAdapter.getArgumentResolvers();
+        assert argumentResolvers != null;
+        argumentResolvers.stream()
+                .filter(argumentResolver -> argumentResolver instanceof RequestResponseBodyMethodProcessor)
+                .forEach(argumentResolver -> requestResponseBodyMethodProcessor = (RequestResponseBodyMethodProcessor) argumentResolver);
+        argumentResolvers.stream()
+                .filter(argumentResolver -> argumentResolver instanceof ServletModelAttributeMethodProcessor)
+                .forEach(argumentResolver -> servletModelAttributeMethodProcessor = (ServletModelAttributeMethodProcessor) argumentResolver);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void afterSingletonsInstantiated() {
+        this.requestMappingHandlerAdapter = applicationContext.getBean(RequestMappingHandlerAdapter.class);
     }
 }
 
