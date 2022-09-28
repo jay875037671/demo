84443a358a516fc3a38076afa52a71f67fe2a628
likj <jay.likangjun@gmail.com>
2022-09-28 13:33:14 +0800

    fix(demo):代码修改

--- eureka-consumer/src/main/java/com/example/eurekaconsumer/DemoController.java
+++ eureka-consumer/src/main/java/com/example/eurekaconsumer/DemoController.java
@@ -1,11 +1,17 @@
 package com.example.eurekaconsumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @Slf4j
@@ -20,8 +26,15 @@ public class DemoController {
         return "name：" + name + "\nage：" + age;
     }
 
-    public String doPost(String str, HttpServletRequest request) {
+    public String doPost(Map<String, Object> params, HttpServletRequest request) {
         String contentType = request.getHeader("content-type");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String str = null;
+        try {
+            str = objectMapper.writeValueAsString(params);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         log.info("入参值：{}", str);
         String req = String.format("Content-Type: %s\nparam: %s", contentType, str);
         String url = "http://EUREKA-PRODUCER/demo?param={req}";
@@ -29,8 +42,18 @@ public class DemoController {
         return restTemplate.getForObject(url, String.class, req);
     }
 
-    @RequestMapping("/rest")
-    public String rest(@MultiArgumentResolver String str, HttpServletRequest request) {
-        return doPost(str, request);
+    @RequestMapping(value = "/rest",consumes = "application/json")
+    public String restJson(@MultiArgumentResolver Map<String, Object> params, HttpServletRequest request) {
+        return doPost(params, request);
+    }
+
+    @RequestMapping(value = "/rest",consumes = "application/x-www-form-urlencoded")
+    public String restForm(Map<String, Object> params, HttpServletRequest request) {
+        return doPost(params, request);
+    }
+
+    @RequestMapping(value = "/rest",consumes = "multipart/form-data")
+    public String restFormData(Map<String, Object> params, HttpServletRequest request) {
+        return doPost(params, request);
     }
 }
