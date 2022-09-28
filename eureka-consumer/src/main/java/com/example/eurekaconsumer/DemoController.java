5fb0d24fba3c8e1a55805f815ae54291dee43c3a
likj <jay.likangjun@gmail.com>
2022-09-28 14:58:11 +0800

    fix(demo):三种方式实现json和form同一个url调用

--- eureka-consumer/src/main/java/com/example/eurekaconsumer/DemoController.java
+++ eureka-consumer/src/main/java/com/example/eurekaconsumer/DemoController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
