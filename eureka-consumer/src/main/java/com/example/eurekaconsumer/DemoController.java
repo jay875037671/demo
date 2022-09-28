ec8095160896908068fe96d578cdb99a63ef55ec
likj <jay.likangjun@gmail.com>
2022-09-28 14:15:51 +0800

    fix(demo):代码修改

--- eureka-consumer/src/main/java/com/example/eurekaconsumer/DemoController.java
+++ eureka-consumer/src/main/java/com/example/eurekaconsumer/DemoController.java
@@ -3,15 +3,12 @@ package com.example.eurekaconsumer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.entity.ContentType;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
-import java.util.Objects;
 
 @RestController
 @Slf4j
@@ -26,15 +23,20 @@ public class DemoController {
         return "name：" + name + "\nage：" + age;
     }
 
-    public String doPost(Map<String, Object> params, HttpServletRequest request) {
+    @RequestMapping(value = "/rest")
+    public String restJson(@MultiArgumentResolver DemoDto dto, HttpServletRequest request) {
+        return doPost(dto, request);
+    }
+
+    @RequestMapping(value = "/test")
+    public String restJson(@MultiArgumentResolver @RequestParam(value = "file",required = false) String formStr,
+                           @RequestBody(required = false) String jsonStr,
+                           HttpServletRequest request) {
+        return doPost(formStr,jsonStr, request);
+    }
+
+    private String doPost(String str, HttpServletRequest request){
         String contentType = request.getHeader("content-type");
-        ObjectMapper objectMapper = new ObjectMapper();
-        String str = null;
-        try {
-            str = objectMapper.writeValueAsString(params);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
         log.info("入参值：{}", str);
         String req = String.format("Content-Type: %s\nparam: %s", contentType, str);
         String url = "http://EUREKA-PRODUCER/demo?param={req}";
@@ -42,18 +44,24 @@ public class DemoController {
         return restTemplate.getForObject(url, String.class, req);
     }
 
-    @RequestMapping(value = "/rest",consumes = "application/json")
-    public String restJson(@MultiArgumentResolver Map<String, Object> params, HttpServletRequest request) {
-        return doPost(params, request);
+    public String doPost(DemoDto dto, HttpServletRequest request) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String str = "";
+        try {
+            str = objectMapper.writeValueAsString(dto);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return doPost(str,request);
     }
 
-    @RequestMapping(value = "/rest",consumes = "application/x-www-form-urlencoded")
-    public String restForm(Map<String, Object> params, HttpServletRequest request) {
-        return doPost(params, request);
-    }
-
-    @RequestMapping(value = "/rest",consumes = "multipart/form-data")
-    public String restFormData(Map<String, Object> params, HttpServletRequest request) {
-        return doPost(params, request);
+    private String doPost(String formStr, String jsonStr, HttpServletRequest request) {
+        String str = "";
+        if(StringUtils.isNotBlank(jsonStr)){
+           str = jsonStr;
+        }else if(StringUtils.isNotBlank(formStr)){
+            str = formStr;
+        }
+        return doPost(str,request);
     }
 }
---
+++ eureka-consumer/src/main/java/com/example/eurekaconsumer/DemoDto.java
@@ -0,0 +1,10 @@
+package com.example.eurekaconsumer;
+
+import lombok.Data;
+
+@Data
+public class DemoDto {
+    private String name;
+    private String age;
+    private String id;
+}
